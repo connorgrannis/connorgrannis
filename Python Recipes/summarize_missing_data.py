1b2c23dc6c5c69705e83010d44df640ae8472518
connorgrannis <48933438+connorgrannis@users.noreply.github.com>
2023-03-25 14:38:22 -0400

    Create summarize_missing_data.py

---
+++ Python Recipes/summarize_missing_data.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+def missing_values_table(df):
+        mis_val = df.isnull().sum()
+        mis_val_percent = 100 * df.isnull().sum() / len(df)
+        mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+        mis_val_table_ren_columns = mis_val_table.rename(
+        columns = {0 : 'Missing Values', 1 : '% of Total Values'})
+        mis_val_table_ren_columns = mis_val_table_ren_columns[
+            mis_val_table_ren_columns.iloc[:,1] != 0].sort_values(
+        '% of Total Values', ascending=False).round(1)
+        print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"      
+            "There are " + str(mis_val_table_ren_columns.shape[0]) +
+              " columns that have missing values.")
+        return mis_val_table_ren_columns
+
+
+# Get the columns with > 50% missing
+missing_df = missing_values_table(df)
+print(missing_df)
+missing_columns = list(missing_df[missing_df['% of Total Values'] > 50].index)
+print(f'We will remove %d column(s): {", ".join(missing_columns)}' % len(missing_columns))
